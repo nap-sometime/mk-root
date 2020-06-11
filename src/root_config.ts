e43673efb1ec413dae423d73b8ff2ace7b475af2
worasitipon <worasitipon.t@hotmail.com>
2020-06-11 14:17:45 +0700

    add @mk/auth app

--- src/root_config.ts
+++ src/root_config.ts
@@ -1,16 +1,24 @@
 import { registerApplication, start } from 'single-spa';
 
+// const add = (publicName: string, options: Omit<RegisterApplicationConfig, 'name' | 'app'> = { activeWhen: ['/'] }) => ({
+// 	name: publicName,
+// 	app: () => System.import(publicName),
+// 	...options,
+// });
+
 registerApplication({
 	name: '@single-spa/welcome',
 	app: () => System.import('https://unpkg.com/single-spa-welcome/dist/single-spa-welcome.js'),
 	activeWhen: ['/'],
 });
 
-// registerApplication({
-//   name: "@mk/navbar",
-//   app: () => System.import("@mk/navbar"),
-//   activeWhen: ["/"]
-// });
+registerApplication({
+	name: '@mk/auth',
+	app: () => System.import('@mk/auth'),
+	activeWhen: ['/'],
+});
+
+// [add('@single-spa/welcome')].forEach(registerApplication);
 
 start({
 	urlRerouteOnly: true,
