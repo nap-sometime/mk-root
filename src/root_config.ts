155717ab38b744115371208025d4fb3047a971eb
worasitipon <worasitipon.t@hotmail.com>
2020-06-11 19:03:35 +0700

    update register app

--- src/root_config.ts
+++ src/root_config.ts
@@ -9,7 +9,7 @@ import { registerApplication, start } from 'single-spa';
 registerApplication({
 	name: '@single-spa/welcome',
 	app: () => System.import('https://unpkg.com/single-spa-welcome/dist/single-spa-welcome.js'),
-	activeWhen: ['/'],
+	activeWhen: ['/home'],
 });
 
 registerApplication({
